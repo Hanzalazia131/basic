05e6e64ce8bb3537fa09f8180a629b4728a666e1
Hanzala-Reachout <hanzala@reachout.cloud>
2023-01-19 17:56:50 +0500

    Server Working for Get And Push

--- backend/routes/Infos.js
+++ backend/routes/Infos.js
@@ -1,16 +1,18 @@
-const router = require('express');
+const router = require('express').Router();
 let Info = require('../Models/Info.model');
 
-router.Router('/').get((req, res) => {
+
+
+router.route('/').get((req, res) => {
     Info.find()
     .then(Infos => res.json(Infos))
     .catch(err => res.status(400).json('Error: ' + err))    
 });
 
-router.Router('/add').post((req, res) => {
-    const Name = req.body.Name;
-    const CNIC = req.body.CNIC;
-    const DOB = req.body.DOB;
+router.route('/add').post((req, res) => {
+    const Name = req.Name;
+    const CNIC = req.CNIC;
+    const DOB = req.DOB;
 
     const newInfo = new Info({
         Name,
--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,14 @@ const app = express();
 const port = process.env.port || 5000;
 
 app.use(cors());
-app.use(express.json());
-
+// app.use(express.json());
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+// });
 const uri = process.env.ATLAS_URI;
 
 mongoose.set("strictQuery", false); 
@@ -22,7 +28,8 @@ connection.once('open', () => {
 
 const InfoRouter = require('./routes/Infos');
 
-app.use('/test',InfoRouter);
+
+app.use('/Infos',InfoRouter);
 
 app.listen(port, () => {
     console.log(`server is running on the Port:  ${port}`);
